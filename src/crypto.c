b80475a53298fbe74d713a825a0a7feabfe369d1
seb-link <seb974.gamer.pro@gmail.com>
2025-02-26 17:51:27 +0400

    update 	modified:   src/crypto.c

--- src/crypto.c
+++ src/crypto.c
@@ -25,9 +25,11 @@ int crypto_init(void) {
 
 challenge* generate_challenge(void) {
   size_t len = 64;
-  unsigned char* hash =   NULL;
-  unsigned char* rand =   malloc(len);
-  challenge*     result = NULL;
+  unsigned char*  hash =   NULL;
+  unsigned char*  rand =   malloc(sizeof(unsigned char*) * len);
+  challenge*      result;
+  result->rand = NULL;
+  result->hash = NULL;
   // Generate 64 bytes of random data
   int a = get_random_bytes(&rand, len);
   if (a < 0)
@@ -43,26 +45,39 @@ challenge* generate_challenge(void) {
 }
 
 // Generate cryptographically secure random bytes
+// Generate cryptographically secure random bytes
 int get_random_bytes(unsigned char **buffer, size_t length) {
-  int fd = open("/dev/urandom", O_RDONLY);
-  if (fd == -1) {
-      perror("open");
+  *buffer = malloc(length);
+  if (*buffer == NULL) {
+      perror("malloc");
       return -1;
   }
 
-  ssize_t result = read(fd, &buffer, length);
+  int fd = open("/dev/urandom", O_RDONLY);
+  if (fd == -1) {
+      perror("open");
+      free(*buffer);
+      return -1;
+  }
+
+  ssize_t result = read(fd, *buffer, length);
   if (result == -1) {
        perror("read");
        close(fd);
+       free(*buffer);
        return -1;
   } else if (result == 0) {
     fprintf(stderr, "Unexpected EOF reading /dev/urandom\n");
     close(fd);
+    free(*buffer);
     return -1;
   }
 
-  if (result != length)
+  if (result != length) {
+    free(*buffer);
+    close(fd);
     return -1;
+  }
   
   close(fd);
   return 0;
