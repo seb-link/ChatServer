c03f3781aaea4a22cc29d8c935295df0bf5bb0bf
seb-link <seb974.gamer.pro@gmail.com>
2025-03-06 19:42:52 +0400

    print_hex is the only way i know it's random

--- tests/src/randomtest.c
+++ tests/src/randomtest.c
@@ -3,12 +3,12 @@
 #include "common.h"
 
 int main(void) {
-  int a = 58;
+  int a = 64;
   char *rand = malloc(a);
   for (int i = 0; i<TEST_NUM; ++i) {
     bzero(rand,a);
     get_random_bytes(&rand, a);
-    print_hex(rand,a);
+    print_hex(rand,a); // The only way i found to check if random
   }
   return 0;
 }
