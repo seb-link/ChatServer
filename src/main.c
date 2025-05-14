3a5d746044ad498ac2edd7f79a156d2bcda1ce46
seb-link <seb974.gamer.pro@gmail.com>
2025-05-14 14:11:46 +0400

    main.c : Made configFilePath null-terminated 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -9,14 +9,16 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *configFilePath = malloc(CONFIG_NAME_LENGTH);
+  char *configFilePath = malloc(CONFIG_NAME_LENGTH + 1);
   
   if (argc != 2) {
     configFilePath = "config.ini";
   } else {
     snprintf(configFilePath, CONFIG_NAME_LENGTH, "%s", argv[1]);
   }
-  
+
+  configFilePath[CONFIG_NAME_LENGTH + 1] = '\0';
+
   run(configFilePath);
   
   free(configFilePath);
