907e560a507661b6b54cd3689d9c75d63085a898
seb-link <seb974.gamer.pro@gmail.com>
2025-05-14 22:53:44 +0400

    Removed debug print statement 	modified:   src/log.c

--- src/log.c
+++ src/log.c
@@ -35,7 +35,6 @@ void log_close(void) {
 
 void log_msg(LogLevel level, const char* msg, ...) {
   if (!log_file) return;
-  printf("config : %zu, level : %zu, %d\n", app_config.logLevel ,level, app_config.logLevel < level);
   if (app_config.logLevel < level) return; 
   
   va_list args;
